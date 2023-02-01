eb4a0b2376fa6418dc0e1862b965961dc0af8283
Patryk Zwolak <patrykzwolak.pz@gmail.com>
2023-02-01 12:37:56 +0100

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TestInterface} from 'types'
 
 function App() {
+/*   const foo: TestInterface = {
+    x: 1,
+  } */
   return (
     <div className="App">
       <header className="App-header">
